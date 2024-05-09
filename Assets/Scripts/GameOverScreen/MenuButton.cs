fedf20b311dca18e5e98c9442705be5c699aca56
keonipackard <keonipackard@gmail.com>
2024-05-08 17:53:46 -0700

    5/8
    - added more bots
    - audio sounds for bots
    - game over screen

---
+++ Assets/Scripts/GameOverScreen/MenuButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public void MainMenu() {
+        Debug.Log("Loading Main Menu...");
+        //SceneManager.LoadScene("________");  // <------INSERT MENU CANVAS / SCENE NAME HERE
+    }
+}
--- Assets/Scripts/Nextbot/NextbotAI.cs
+++ Assets/Scripts/Nextbot/NextbotAI.cs
@@ -1,28 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NextBotController : MonoBehaviour {
-    public Transform player; // reference to player
-    public float moveSpeed = 5f; // movement speed of nextbot
-    public float followDistance = 10f; // distance that nextbot starts following player
-    public float collisionDistance = 1f; // distance to trigger collision with player
-
-    private bool playerAlive = true; // to track player life status
+public class NextbotAI : MonoBehaviour {
+    public Transform player;
+    public float followSpeed = 5.0f; // chasing player speed of nextbot
+    public float wanderspeed = 3.0f; // wandering speed of nextbot
+    public float followDistance = 10.0f; // distance that nextbot starts following player
+    public float collisionDistance = 1.0f; // distance to trigger collision with player
+    public float obstacleRange = 5.0f; // distance from obstacles where nextbot will turn 
+    public bool playerAlive = true;
 
     private void Update() {
-        if (playerAlive) {
-            // checks if player is within follow distance
-            if (IsPlayerWithinFollowDistance()) {
-                FollowPlayer();
+        // checks if player is within follow distance
+        if (IsPlayerWithinFollowDistance()) {
+            FollowPlayer();
+            transform.LookAt(player);
+        } else {
+            // if player is not within follow range (WanderingAI script)
+            transform.Translate(0, 0, wanderspeed * Time.deltaTime);
+
+            // create a ray from the wandering game object, pointed in the 
+            // same direction as that game object
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            // data container w/ hit info 
+            RaycastHit hit;
+
+            // perform a raycast w/ a circular volume around the ray 
+            if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                // get a reference to the game object that was hit
+                // by the sphere cast
+                GameObject hitObject = hit.transform.gameObject;
+
+                if (hit.distance < obstacleRange) {
+                    // if the ray had hit something, turn around by a random angle
+                    // only perform if the object is within the obstacleRange
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
             }
         }
     }
 
+    // checks if player is within follow distance
     private bool IsPlayerWithinFollowDistance() {
         return Vector3.Distance(transform.position, player.position) <= followDistance;
     }
 
+    // tells nextbot ai to follow player / checks collision with player
     private void FollowPlayer() {
         if (player != null) {
             // calculates direction to move towards the player
@@ -30,34 +57,13 @@ public class NextBotController : MonoBehaviour {
             direction.Normalize();
 
             // moves nextbot towards the player
-            transform.position += direction * moveSpeed * Time.deltaTime;
+            transform.position += direction * followSpeed * Time.deltaTime;
 
-            // locks nextbot rotation along X and Z axes
-            Vector3 eulerAngles = transform.rotation.eulerAngles;
-            transform.rotation = Quaternion.Euler(0f, eulerAngles.y, 0f);
-
-            // checks collision with player
+            // checks collision with player & if collide sends player to game over scene
             if (Vector3.Distance(transform.position, player.position) <= collisionDistance) {
-                PlayerDies();
+                playerAlive = false;
+                SceneManager.LoadScene("GameOver");
             }
-        }
-    }
-
-    private void PlayerDies() {
-        playerAlive = false;
-        Debug.Log("You have Died");
-        /*
-        Rigidbody playerRigidbody = player.GetComponent<Rigidbody>();
-        if (playerRigidbody != null) {
-            playerRigidbody.velocity = Vector3.zero; // Stop player's movement
-            playerRigidbody.constraints = RigidbodyConstraints.FreezeAll; // Freeze player's movement
-        }
-        */
-
-        // disables player controller script
-        CharacterController playerController = player.GetComponent<CharacterController>();
-        if (playerController != null) {
-            playerController.enabled = false;
         }
     }
 }
