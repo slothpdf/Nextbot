02af6a4ba8a766050c8c4d0cab59a632ff131511
keonipackard <keonipackard@gmail.com>
2024-05-08 21:48:29 -0700

    5/8
    - page files from davids branch added

---
+++ Assets/Scripts/Pages/CollectPages.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPages : MonoBehaviour
+{
+    public GameObject collectText;
+
+    public AudioSource collectSound;
+
+    private GameObject page;
+
+    private bool inReach;
+
+    private GameObject gameLogic;
+
+    void Start()
+    {
+        collectText.SetActive(false);
+
+        inReach = false;
+
+        page = this.gameObject;
+
+        gameLogic = GameObject.FindWithTag("GameLogic");
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            collectText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            collectText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if(inReach && Input.GetButtonDown("pickup"))
+        {
+            gameLogic.GetComponent<GameLogic>().pageCount += 1;
+            collectSound.Play();
+            collectText.SetActive(false);
+            page.SetActive(false);
+            inReach = false;
+        }
+
+        
+    }
+}
