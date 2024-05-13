4c2c2c62a0e52cd55175002f7c63fff68a5343a4
adrianazper <adrianazper11@gmail.com>
2024-05-12 21:58:57 -0700

    PausedMenu

---
+++ Assets/Scripts/PausedMenu/PausedMenu.cs
@@ -0,0 +1,80 @@
+using Palmmedia.ReportGenerator.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedMenu : MonoBehaviour
+{
+    public GameObject pausedMenu;
+    public GameObject settings; 
+    public PlayerControls playerControls;
+
+    public bool paused = false;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused == false)
+            {
+                pausedMenu.SetActive(true);
+                paused = true;
+
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                if (playerControls != null)
+                {
+                    playerControls.enabled = false;
+                }
+            }
+
+            else if (paused == true)
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pausedMenu.SetActive(false);
+        paused = false;
+
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if(playerControls != null)
+        {
+            playerControls.enabled = true;
+        }
+
+ 
+    }
+
+    public void GoToMenu(string MainMenu)
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void RestartGameButton(string world)
+    {
+        SceneManager.LoadScene("world");
+        ResumeGame();
+    }
+
+    public void OpenSettings()
+    {
+        if (settings != null)
+        {
+            settings.SetActive(true);
+        }
+    }
+}
